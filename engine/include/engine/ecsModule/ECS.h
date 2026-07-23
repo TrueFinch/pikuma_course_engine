5ed52fea443bcf7fd260cc36c7191a4913dbcfe2
Vladimir Glushkov <gvovak@gmail.com>
2026-07-23 15:28:27 +0500

    ECS: extend struct Entity, add EntityManager
    Entity:
    * 'value' field now consists of two parts 'id' and 'generation' and is hidden under protected
    * Also 'value' is forbidden to cast Entity to ValueType and create new Entity
    * Add std::hash specialization to add ability to use Entity as key in associative containers
    EntityManager — a class for managing created entities. Creates and destroys entities, provides information about the relevance of the entity identifier (isAlive). Provides a mechanism for reusing entities using a generation counter

--- engine/include/engine/ecsModule/ECS.h
+++ engine/include/engine/ecsModule/ECS.h
@@ -5,16 +5,79 @@
 #pragma once
 
 #include <bitset>
+#include <vector>
 
 #include "engine/utilsModule/Types.h"
 
 namespace pce {
+	namespace details {
+		constexpr uint32 MAX_COMPONENTS = 32;
+		using Signature = std::bitset<MAX_COMPONENTS>;
+	}
+
+	class EntityManager;
+
 	struct Entity {
+		friend EntityManager;
+		friend std::hash<Entity>;
 		using ValueType = uint32;
 
-		ValueType value;
+	protected:
+		ValueType m_value;
+
+		static constexpr ValueType INDEX_BITS = 20;
+		static constexpr ValueType GENERATION_BITS = 12;
+		static constexpr ValueType INDEX_MASK = (1u << INDEX_BITS) - 1u;
+
+	public:
+		constexpr Entity(const Entity& other) noexcept = default;
+
+		constexpr Entity& operator=(const Entity& other) noexcept = default;
+
+		constexpr Entity(Entity&& other) noexcept = default;
+
+		constexpr Entity& operator=(Entity&& other) noexcept = default;
+
+		[[nodiscard]] constexpr ValueType GetIndex() const noexcept {
+			return m_value & INDEX_MASK;
+		}
+
+		[[nodiscard]] constexpr ValueType GetGeneration() const noexcept {
+			return m_value >> INDEX_BITS;
+		}
+
+		constexpr explicit operator ValueType() const noexcept = delete;
 
 		constexpr auto operator<=>(const Entity&) const noexcept = default;
+
+	private:
+		static constexpr Entity MakeEntity(const ValueType index, const ValueType generation) noexcept {
+			return Entity((index & INDEX_MASK) | (generation << INDEX_BITS));
+		}
+
+		constexpr Entity() noexcept = default;
+
+		constexpr explicit Entity(const ValueType v) noexcept: m_value(v) {}
+	};
+
+	class EntityManager {
+	public:
+		Entity CreateEntity();
+
+		void DestroyEntity(const Entity& entity);
+
+		void SetSignature(const Entity& entity, const details::Signature& signature);
+
+		[[nodiscard]] details::Signature& GetSignature(const Entity& entity);
+
+		[[nodiscard]] bool IsAlive(const Entity& entity) const;
+
+		[[nodiscard]] const details::Signature& GetSignature(const Entity& entity) const;
+
+	private:
+		std::vector<uint32> m_generations;
+		std::vector<uint32> m_freeList;
+		std::vector<details::Signature> m_entityComponentSignatures;
 	};
 
 
@@ -51,3 +114,8 @@ namespace pce {
 		virtual void FixedUpdate(float dt) = 0;
 	};
 }
+
+template<>
+struct std::hash<pce::Entity> {
+	size_t operator()(const pce::Entity& entity) const noexcept;
+};
--- engine/src/ecsModule/ECS.cpp
+++ engine/src/ecsModule/ECS.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "engine/ecsModule/ECS.h"
+
+pce::Entity pce::EntityManager::CreateEntity() {
+	uint32 freeIndex;
+	if (!m_freeList.empty()) {
+		freeIndex = m_freeList.back();
+		m_freeList.pop_back();
+		m_entityComponentSignatures[freeIndex].reset();
+	} else {
+		freeIndex = m_generations.size();
+		m_generations.push_back(0);
+		m_entityComponentSignatures.emplace_back();
+	}
+	return Entity::MakeEntity(freeIndex, m_generations[freeIndex]);
+}
+
+void pce::EntityManager::DestroyEntity(const Entity& entity) {
+	const auto index = entity.GetIndex();
+	m_freeList.push_back(index);
+	++m_generations[index];
+	m_entityComponentSignatures[index].reset();
+}
+
+void pce::EntityManager::SetSignature(const Entity& entity, const details::Signature& signature) {
+	m_entityComponentSignatures[entity.GetIndex()] = signature;
+}
+
+pce::details::Signature& pce::EntityManager::GetSignature(const Entity& entity) {
+	return m_entityComponentSignatures[entity.GetIndex()];
+}
+
+bool pce::EntityManager::IsAlive(const Entity& entity) const {
+	const auto index = entity.GetIndex();
+	const auto gen = entity.GetGeneration();
+
+	return index < m_generations.size() && m_generations[index] == gen;
+}
+
+const pce::details::Signature& pce::EntityManager::GetSignature(const Entity& entity) const {
+	return m_entityComponentSignatures[entity.GetIndex()];
+}
+
+size_t std::hash<pce::Entity>::operator()(const pce::Entity& entity) const noexcept {
+	return std::hash<pce::Entity::ValueType>{}(entity.m_value);
+}
